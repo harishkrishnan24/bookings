4a56c7707c3db2dd98b572034ae2b0ceaf532537
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-12 21:12:27 +0530

    forms pkg test

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,7 +2,6 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
@@ -21,8 +20,8 @@ func New(data url.Values) *Form {
 	}
 }
 
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
 	if x == "" {
 		return false
 	}
@@ -42,8 +41,8 @@ func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
 }
 
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
